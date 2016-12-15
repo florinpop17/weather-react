a4e4e0ce90defb7d942affd22f398033eefee1c5
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 20:37:12 +0200

    removed https check

--- server.js
+++ server.js
@@ -4,13 +4,6 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(function(req, res, next) {
-    if(req.headers['x-forwarded-proto'] === ['http']) {
-        next();
-    } else {
-        res.redirect('http://' + req.hostname + req.url);
-    }
-});
 
 app.use(express.static('public'))
 
