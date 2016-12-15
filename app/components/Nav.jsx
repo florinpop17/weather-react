db6c1d5919a24a640027a7247bba82ec30906b18
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 21:53:55 +0200

    added styles to the nav-bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,26 +1,21 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-//var Nav = React.createClass({
-//    render: function() {
-//        return (
-//            <div>
-//                <h2>Nav component</h2>
-//                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-//                <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-//                <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-//            </div>
-//        );
-//    }
-//});
 
 var Nav = (props) => {
     return (
-        <div>
-            <h2>Nav component</h2>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">Weather app</li>
+                    <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+                    <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+                    <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+            
+            </div>
         </div>
     );
 }
