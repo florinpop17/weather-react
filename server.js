510dbee1936a7cb9192624ef9249066f96bb9027
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 20:55:11 +0200

    changed http if statement

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 
-//app.use(function(req, res, next) {
-//    if(req.headers['x-forwarded-proto'] === 'http') {
-//        next();
-//    } else {
-//        res.redirect('http://' + req.hostname + req.url);
-//    }
-//});
+app.use(function(req, res, next) {
+    if(req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
+    }
+});
 
 app.use(express.static('public'))
 
