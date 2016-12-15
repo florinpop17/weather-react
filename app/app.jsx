2e33607d5e1bb9ae294b4fa3504cc8cb3aa825ca
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 10:03:09 +0200

    changed h1

--- app/app.jsx
+++ app/app.jsx
@@ -2,8 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-
 ReactDOM.render(
-    <h1>Welcome to my react boilerplate!</h1>, 
+    <h1>Welcome to my Weather app!</h1>, 
     document.getElementById("react-container")
 );
