b4ea1f3b2c01c2f09a3147ebbd039093be91a6f7
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 20:58:43 +0200

    renamed examples component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,10 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-        <h3>Examples Component</h3>
+        <div>
+            <h3>Examples</h3>
+            <p>Welcome to the examples page.</p>
+        </div>
     );
 }
 
