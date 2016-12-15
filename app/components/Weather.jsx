30c25836efd1f1661eae4891a5787e270b958312
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 12:51:45 +0200

    added getInitialState function

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -3,10 +3,19 @@ var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 
 var Weather = React.createClass({
-   handleSearch : function (location) {
-       alert(location);
-   },
-   render: function() {
+    getInitialState: function () {
+        return {
+            location: 'Miami',
+            temp: 88
+        }  
+    },
+    handleSearch : function (location) {
+       this.setState({
+            location : location,
+            temp: 23
+       });
+    },
+    render: function() {
        return (
            <div>
                <h3>Weather Component</h3>
@@ -14,7 +23,7 @@ var Weather = React.createClass({
                <WeatherMessage/>
            </div>
        );
-   } 
+    } 
 });
 
 module.exports = Weather;
--- public/bundle.js
+++ public/bundle.js
@@ -26472,8 +26472,17 @@
 	var Weather = React.createClass({
 	    displayName: 'Weather',
 
+	    getInitialState: function getInitialState() {
+	        return {
+	            location: 'Miami',
+	            temp: 88
+	        };
+	    },
 	    handleSearch: function handleSearch(location) {
-	        alert(location);
+	        this.setState({
+	            location: location,
+	            temp: 23
+	        });
 	    },
 	    render: function render() {
 	        return React.createElement(
