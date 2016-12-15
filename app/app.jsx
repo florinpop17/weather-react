35a7e07ed3ee2f7a64226fcef65e453ed657f74f
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 10:00:10 +0200

    added react-router

--- app/app.jsx
+++ app/app.jsx
@@ -1,4 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-ReactDOM.render(<h1>Welcome to my react boilerplate!</h1>, document.getElementById("react-container"));
+
+ReactDOM.render(
+    <h1>Welcome to my react boilerplate!</h1>, 
+    document.getElementById("react-container")
+);
