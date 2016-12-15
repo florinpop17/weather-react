e228085e5a67033a5dcb11474d7c634e0c619199
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 10:11:47 +0200

    added nav component

---
+++ app/components/Nav.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Nav = React.createClass({
+    render: function() {
+        return (
+            <h2>Nav component</h2>
+        );
+    }
+});
+
+module.exports = Nav;
