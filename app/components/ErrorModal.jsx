4a4ddbb96c15fa72ba94bea76065c3d8d065a04c
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 22:48:05 +0200

    added props for title and message for modal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,15 +1,28 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
+    getDefaultProps: function() {
+        return {
+            title: 'Error'
+        };
+    },
+    
+    propTypes: {
+        title: react.PropTypes.string,
+        message: react.PropTypes.string.isRequired
+    },
+    
     componentDidMount: function() {
         var modal = new Foundation.Reveal($('#error-modal'));
         modal.open();
     },
+    
     render: function() {
+        var {title, message} = this.props;
         return (
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-                <h4>Some title</h4>
-                <p>Error message</p>
+                <h4>{title}</h4>
+                <p>{message}</p>
                 <p>
                     <button className="button hollow" data-close="">Ok</button>
                 </p>
