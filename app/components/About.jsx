4dbd64c1db6ba5c689954055c270c083df1c507d
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 22:15:19 +0200

    added content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,9 +9,20 @@ var About = React.createClass({
 });
 
 var About = () => (
-    <div>
-        <h3>About</h3>
+    <div className="text-center">
+        <h1>About</h1>
         <p>This is a simple react app that shows the weather in different locations.</p>
+        <p>Built after <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/">this</a> udemy course.</p>
+        <p>Some tools used:</p>
+        <ol>
+            <li>Reactjs</li>
+            <li>Nodejs</li>
+            <li>Foundation</li>
+            <li>OpenWeatherMap</li>
+            <li>Webpack</li>
+            <li>Babel</li>
+            <li>Express</li>
+        </ol>
     </div>
 );
 
