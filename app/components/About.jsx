f69867b55eac93694b07b06c2ffbea0ae23c6163
Florin Pop <popflorin1705@yahoo.com>
2016-12-15 10:56:45 +0200

    added components

---
+++ app/components/About.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var About = React.createClass({
+   render: function() {
+       return (
+           <h3>About Component</h3>
+       );
+   } 
+});
+
+module.exports = About;
---
+++ app/components/Weather.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Weather = React.createClass({
+   render: function() {
+       return (
+           <h3>Weather Component</h3>
+       );
+   } 
+});
+
+module.exports = Weather;
